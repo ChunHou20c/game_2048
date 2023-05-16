90fd1e072275a736e45314eefb9c1c6f2e2ff1c3
chunhou20c <youmustbekiddingmeshit@gmail.com>
2023-05-16 15:27:19 +0800

    make the first function

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn hello() -> String {
+    String::from("Hello World from wasm!")
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
